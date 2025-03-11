3bafd4f2233c928b07984873a694d5c1bdb0aea3
tiendat3008 <trantiendat1679@gmail.com>
2025-03-11 10:14:26 +0700

    trailer repo

--- src/main/java/com/backend/repository/TrailerRepository.java
+++ src/main/java/com/backend/repository/TrailerRepository.java
@@ -8,7 +8,7 @@ import com.backend.entity.Trailer;
 import java.util.List;
 
 @Repository
-public interface TrailerRepository extends JpaRepository<Trailer, Long> {
+public interface TrailerRepository extends JpaRepository<Trailer, String> {
 
     List<Trailer> findByTvSerie_Id(Long seriesId);
 

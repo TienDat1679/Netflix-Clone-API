61b813df60114fddb02b5381c3a140db7d053f2d
tiendat3008 <trantiendat1679@gmail.com>
2025-03-25 23:55:46 +0700

    add role, permission

---
+++ src/main/java/com/backend/controller/PermissionController.java
@@ -0,0 +1,49 @@
+package com.backend.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.dto.ApiResponse;
+import com.backend.dto.request.PermissionRequest;
+import com.backend.dto.response.PermissionResponse;
+import com.backend.service.PermissionService;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@RestController
+@RequestMapping("/permissions")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class PermissionController {
+    PermissionService permissionService;
+
+    @PostMapping
+    ApiResponse<PermissionResponse> create(@RequestBody PermissionRequest request) {
+        return ApiResponse.<PermissionResponse>builder()
+                .result(permissionService.create(request))
+                .build();
+    }
+
+    @GetMapping
+    ApiResponse<List<PermissionResponse>> getAll() {
+        return ApiResponse.<List<PermissionResponse>>builder()
+                .result(permissionService.getAll())
+                .build();
+    }
+
+    @DeleteMapping("/{permission}")
+    ApiResponse<Void> delete(@PathVariable String permission) {
+        permissionService.delete(permission);
+        return ApiResponse.<Void>builder()
+                .build();
+    }
+}
---
+++ src/main/java/com/backend/controller/RoleController.java
@@ -0,0 +1,49 @@
+package com.backend.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.dto.ApiResponse;
+import com.backend.dto.request.RoleRequest;
+import com.backend.dto.response.RoleResponse;
+import com.backend.service.RoleService;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@RestController
+@RequestMapping("/roles")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class RoleController {
+    RoleService roleService;
+
+    @PostMapping
+    ApiResponse<RoleResponse> create(@RequestBody RoleRequest request) {
+        return ApiResponse.<RoleResponse>builder()
+                .result(roleService.create(request))
+                .build();
+    }
+
+    @GetMapping
+    ApiResponse<List<RoleResponse>> getAll() {
+        return ApiResponse.<List<RoleResponse>>builder()
+                .result(roleService.getAll())
+                .build();
+    }
+
+    @DeleteMapping("/{role}")
+    ApiResponse<Void> delete(@PathVariable String role) {
+        roleService.delete(role);
+        return ApiResponse.<Void>builder()
+                .build();
+    }
+}
